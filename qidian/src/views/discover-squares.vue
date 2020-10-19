dbe02be086104339598ca0f45d749aa6bb773276
Ki1uno <69033115+Ki1uno@users.noreply.github.com>
2020-10-19 09:55:37 +0800

    create discover pages

---
+++ qidian/src/views/discover-squares.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- 发现-广场  -->
+  <div class="discover-squares"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ qidian/src/views/discover.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="discover">
+    <router-link to="/discover-follow">关注</router-link>
+    <router-link to="/discover-squares">广场</router-link>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
