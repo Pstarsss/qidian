5008fe075e665d0861adb8d41ac2f3bf2e9331b4
闪烁咩 <blink83199408@163.com>
2020-10-20 09:16:24 +0800

    SelectBoys

--- qidian/src/views/Selected/SelectBoys.vue
+++ qidian/src/views/Selected/SelectBoys.vue
@@ -1,18 +1,349 @@
 <template>
-   <div class=''>
-     男生内容
-   </div>
+  <div class="">
+    <div class="container">
+      <hr />
+      <!-- 推荐书籍 -->
+      <div class="recommend-list">
+        <span class="recommend-list-left"> 秦汉 </span>
+        <span class="recommend-list-right">
+          从今天起，别叫我阿斗，叫我爸爸
+        </span>
+        <i class="el-icon-arrow-right"></i>
+      </div>
+
+      <!-- 畅销精选 -->
+      <div class="wellsell">
+        <div class="wellsell-title">
+          <h2>畅销精选</h2>
+          <span>
+            更多
+            <i class="el-icon-arrow-right"></i>
+          </span>
+        </div>
+
+        <div class="wellsell-container">
+          <div class="wellsell-container-left">
+            <img
+              src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+              alt=""
+            />
+          </div>
+          <div class="wellsell-container-right">
+            <span class="wellsell-container-right-title">从商二十年</span>
+            <p class="wellsell-desc1">连载 323万字</p>
+            <span class="wellsell-desc2">我不是天生强者，我只是天生要强</span>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <!-- 主编力荐 -->
+      <div class="wellsell">
+        <div class="wellsell-title">
+          <h2>主编力荐</h2>
+          <span>
+            更多
+            <i class="el-icon-arrow-right"></i>
+          </span>
+        </div>
+
+        <div class="wellsell-container">
+          <div class="wellsell-container-left">
+            <img
+              src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+              alt=""
+            />
+          </div>
+          <div class="wellsell-container-right">
+            <span class="wellsell-container-right-title">从商二十年</span>
+            <p class="wellsell-desc1">连载 323万字</p>
+            <span class="wellsell-desc2">我不是天生强者，我只是天生要强</span>
+          </div>
+          <hr />
+        </div>
+      </div>
+
+      <!-- 限时免费 -->
+      <div class="time-free">
+        <h2>限时免费</h2>
+        <span class="free-hour">17</span> : <span class="free-minu">22</span> :
+        <span class="free-sec">54</span>
+
+        <span class="for-more">
+          更多
+          <i class="el-icon-arrow-right"></i>
+        </span>
+
+        <div class="time-free-container">
+          <div class="time-free-items">
+            <div class="time-free-item">
+              <img
+                src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+                alt=""
+              />
+              <p>从骷髅岛开始横推万界</p>
+              <span class="time-free-free">免费</span>
+            </div>
+          </div>
+          <div class="time-free-items">
+            <div class="time-free-item">
+              <img
+                src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+                alt=""
+              />
+              <p>从骷髅岛开始横推万界</p>
+              <span class="time-free-free">免费</span>
+            </div>
+          </div>
+          <div class="time-free-items">
+            <div class="time-free-item">
+              <img
+                src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+                alt=""
+              />
+              <p>从骷髅岛开始横推万界</p>
+              <span class="time-free-free">免费</span>
+            </div>
+          </div>
+          <div class="time-free-items">
+            <div class="time-free-item">
+              <img
+                src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+                alt=""
+              />
+              <p>从骷髅岛开始横推万界</p>
+              <span class="time-free-free">免费</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="time-free-change">
+          <i class="el-icon-refresh">1</i> 换一批
+        </div>
+        <hr />
+      </div>
+      <!-- 新书强推 -->
+      <div class="wellsell">
+        <div class="wellsell-title">
+          <h2>新书强推</h2>
+          <span>
+            更多
+            <i class="el-icon-arrow-right"></i>
+          </span>
+        </div>
+
+        <div class="wellsell-container">
+          <div class="wellsell-container-left">
+            <img
+              src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+              alt=""
+            />
+          </div>
+          <div class="wellsell-container-right">
+            <span class="wellsell-container-right-title">从商二十年</span>
+            <p class="wellsell-desc1">连载 323万字</p>
+            <span class="wellsell-desc2">我不是天生强者，我只是天生要强</span>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <!-- 经典完本 -->
+      <div class="wellsell">
+        <div class="wellsell-title">
+          <h2>畅销精选</h2>
+          <span>
+            更多
+            <i class="el-icon-arrow-right"></i>
+          </span>
+        </div>
+
+        <div class="wellsell-container">
+          <div class="wellsell-container-left">
+            <img
+              src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+              alt=""
+            />
+          </div>
+          <div class="wellsell-container-right">
+            <span class="wellsell-container-right-title">从商二十年</span>
+            <p class="wellsell-desc1">连载 323万字</p>
+            <span class="wellsell-desc2">我不是天生强者，我只是天生要强</span>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <!-- 潜力新书和优质精品 -->
+      <div class="qianli-youzhi">
+        <img src="../../assets/img/SelectBoys/1.png" alt="" />
+        <img
+          class="qianli-youzhi-img2"
+          src="../../assets/img/SelectBoys/2.png"
+          alt=""
+        />
+      </div>
+
+      <!-- 现实精品 -->
+      <div class="wellsell">
+        <div class="wellsell-title">
+          <h2>现实精品</h2>
+          <span>
+            更多
+            <i class="el-icon-arrow-right"></i>
+          </span>
+        </div>
+
+        <div class="wellsell-container">
+          <div class="wellsell-container-left">
+            <img
+              src="http://www.zwdu.com/files/article/image/23/23488/23488s.jpg"
+              alt=""
+            />
+          </div>
+          <div class="wellsell-container-right">
+            <span class="wellsell-container-right-title">从商二十年</span>
+            <p class="wellsell-desc1">连载 323万字</p>
+            <span class="wellsell-desc2">我不是天生强者，我只是天生要强</span>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name:'selectboys',
+  name: "selectboys",
   data() {
-   return {
-   };
-},
-
-}
+    return {};
+  },
+};
 </script>
-<style lang='less' scoped>
+<style scoped>
+hr {
+  border-color: #fefefe;
+  margin-block-end: 0.2em;
+  margin-block-start: 0.3rem;
+  margin-inline-end: 0.3rem;
+  margin-inline-start: 0.3rem;
+}
+.recommend-list {
+  margin-left: 0.3rem;
+  font-size: 18px;
+}
+.recommend-list-right {
+  padding-left: 0.3rem;
+  font-size: 0.25rem;
+  font-weight: 500;
+}
+.recommend-list-left {
+  border-radius: 0.08rem;
+  font-size: 0.2rem;
+  padding: 0.02rem 0.04rem;
+  background: #e6353e;
+  color: white;
+  text-align: center;
+}
+.recommend-list > i {
+  font-size: 0.02rem;
+  float: right;
+  margin-right: 0.3rem;
+}
+.wellsell {
+  font-size: 18px;
+  margin-top: 0.4rem;
+  margin-left: 0.3rem;
+}
+.wellsell-title > h2 {
+  display: inline-block;
+}
+.wellsell-title > span {
+  float: right;
+  margin-top: 0.3rem;
+  margin-right: 0.3rem;
+}
+.wellsell-container > div {
+  display: inline-block;
+}
+.wellsell-container-left > img {
+  height: 1.7rem;
+  border-radius: 0.1rem;
+}
+.wellsell-container-right {
+  vertical-align: top;
+
+  margin-left: 0.2rem;
+}
+.wellsell-container-right-title {
+  font-size: 0.3rem;
+  font-weight: 600;
+}
+.wellsell-desc1,
+.wellsell-desc2 {
+  color: #a2a2a2;
+}
+.time-free {
+  font-size: 18px;
+  margin-left: 0.3rem;
+  margin-top: 0.3rem;
+}
+.time-free > h2 {
+  display: inline-block;
+}
+.time-free > .for-more {
+  float: right;
+  margin-right: 0.3rem;
+  margin-top: 0.3rem;
+}
+.time-free-items {
+  margin-top: 0.1rem;
+}
+.free-hour,
+.free-sec,
+.free-minu {
+  font-size: 0.1rem;
+  padding: 0.07rem;
+  border-radius: 0.02rem;
+  background-color: red;
+  color: white;
+}
+.free-sec,
+.free-minu {
+  background-color: black;
+}
+.free-hour {
+  margin-left: 0.2rem;
+}
+.time-free-item {
+  float: left;
+  width: 23%;
+  margin-right: 0.03rem;
+}
+.time-free-item > img {
+  border-radius: 0.1rem;
+  height: 1.7rem;
+}
+.time-free-free {
+  font-size: 0.15rem;
+  padding: 0.06rem 0.1rem;
+  border: 1px solid red;
+  border-radius: 0.04rem;
+  color: red;
+}
+.time-free-change {
+  color: lightseagreen;
+  font-size: 0.18rem;
+  margin-top: 3.4rem;
+  display: block;
+  text-align: center;
+}
+.qianli-youzhi > img {
+  width: 43%;
+  margin-top: 0.3rem;
+  margin-left: 0.3rem;
+}
+.qianli-youzhi > .qianli-youzhi-img2 {
+  display: inline-block;
+  margin-left: 0;
+  width: 43%;
+}
 </style>
