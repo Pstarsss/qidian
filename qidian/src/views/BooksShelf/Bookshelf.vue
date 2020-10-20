2e793d7d78186e0c182a242068a8ff309db1d3ea
panxing <1377689310@qq.com>
2020-10-20 21:40:39 +0800

    ddd

---
+++ qidian/src/views/BooksShelf/Bookshelf.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <book-crack class="bookcolor"></book-crack>
+    <div class="b-findmore">
+        <button>查找更多精彩作品</button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+
+import BookCrack from '@/components/common/bookcrack/BookCrack.vue'
+export default {
+  name: 'bookshelf',
+  components: {
+    BookCrack
+  },
+  
+}
+</script>
+<style scoped>
+
+.b-findmore{
+      width: 100vw;
+    justify-content: center;
+    padding: 0.3rem;
+    display: inline-flex;
+}
+.b-findmore button{
+      border: 1px solid #f00;
+    padding: 0.1rem 0.14rem;
+    border-radius: 0.25rem;
+    background-color: #fff;
+    color: #f00;
+    font-size: 0.2rem;
+}
+</style>
