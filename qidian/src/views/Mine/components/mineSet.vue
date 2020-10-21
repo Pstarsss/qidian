91f289896d8ce7a63473dd6d6797c99b26131b76
wl0517 <WL0517@dingtalk.com>
2020-10-21 21:09:34 +0800

    mineSet update

---
+++ qidian/src/views/Mine/components/mineSet.vue
@@ -0,0 +1,174 @@
+<template>
+	<div class="mineSet">
+		<div class="title">
+			<i class="el-icon-arrow-left icon-left" @click="back"></i>
+			设置
+		</div>
+		<!-- <scroll :probeType="3" class="wrapper" ref="scroll" @pullingUp="pullingUp"> -->
+		<div class="list1">
+			推送通知
+			<i class="el-icon-arrow-right icon-right"></i>
+		</div>
+		<div class="list1">
+			自动订阅管理
+			<i class="el-icon-arrow-right icon-right"></i>
+		</div>
+		<div class="list2" v-for="(item,index) in list2" :key="index">
+			<span>{{item.title}}</span>
+			<i class="el-icon-arrow-right icon-right"></i>
+			<span class="text">{{item.text}}</span>
+		</div>
+		<div class="list3">
+			隐私设置
+			<i class="el-icon-arrow-right icon-right"></i>
+		</div>
+		<div class="list1">
+			偏好设置
+			<i class="el-icon-arrow-right icon-right"></i>
+		</div>
+		<div class="list2">
+			自动订阅管理
+			<el-switch
+			  v-model="value1"
+			  active-color="gainsboro"
+			  inactive-color="#ff4949" class="switch">
+			</el-switch>
+		</div>
+		<div class="list2">
+			仅用移动网络听书
+			<el-switch
+			  v-model="value2"
+			  active-color="gainsboro"
+			  inactive-color="#ff4949" class="switch">
+			</el-switch>
+		</div>
+		<div class="list1">
+			清除缓存
+			<i class="el-icon-arrow-right icon-right"></i>
+		</div>
+		<div class="list2" v-for="(item,index) in lists" :key="index + '-only'">
+			<span>{{item.title}}</span>
+			<i class="el-icon-arrow-right icon-right"></i>
+			<span class="text">{{item.text}}</span>
+		</div>
+		<!-- </scroll> -->
+		<div class="foot">
+			切换账号
+		</div>
+	</div>
+</template>
+
+<script>
+	// import scroll from '@/components/common/Scroll/scroll.vue'
+	export default{
+		data(){
+			return{
+				list2:[
+					{ title:'自动订阅管理',text:'已关闭'},
+					{ title:'字体设置',text:'汉仪旗黑'},
+					{ title:'阅读设置'}
+				],
+				 value1: true,
+				 value2: true, 
+				 lists:[
+					 { title:'安全中心'},
+					 { title:'青少年模式',text:'未开启'},
+					 { title:'吐个槽'},
+					 { title:'客服与帮助'},
+					 { title:'给我们评分'},
+					 { title:'隐私协议'},
+					 { title:'关于起点读书'} 
+				 ]
+			}
+		},
+		methods:{
+			// pullingUp(){
+			// 	console.log(12);
+			// 	this.$refs.scroll.finishPullUp();
+			// },
+			back(){
+				this.$router.go(-1);
+			}
+		}	
+	}
+</script>
+
+<style scoped>
+	.mineSet{
+		background-color: whitesmoke;
+	}
+	.title{
+		width: 100%;
+		height: 0.7rem;
+		line-height: 0.5rem;
+		background-color: white;
+		position: relative;
+		padding: 0.08rem;
+		z-index: 120;
+		text-align: center;
+		font-size: 0.3rem;
+	}
+	.icon-left{
+		float: left;
+		font-size: 0.35rem;
+		margin-top: 0.05rem;
+		margin-left: 0.1rem;
+	}
+	.list1{
+		width: 100%;
+		height: 0.7rem;
+		line-height: 0.5rem;
+		background-color: white;
+		padding: 0.08rem;
+		text-align: left;
+		font-size: 0.25rem;
+		margin-bottom: 0.1rem;
+	}
+	.icon-right{
+		float: right;
+		font-size: 0.25rem;
+		margin-top: 0.18rem;
+		margin-right: 0.1rem;
+		color: gainsboro;
+	}
+	.list2{
+		width: 100%;
+		height: 0.7rem;
+		line-height: 0.5rem;
+		background-color: white;
+		padding: 0.08rem;
+		text-align: left;
+		font-size: 0.25rem;
+	}
+	.list2 .text{
+		color: gainsboro;
+		font-size: 0.2rem;
+		float: right;
+		margin-top: 0.05rem;
+	}
+	.list3{
+		width: 100%;
+		height: 0.7rem;
+		line-height: 0.5rem;
+		background-color: white;
+		padding: 0.08rem;
+		text-align: left;
+		font-size: 0.25rem;
+		margin-top: 0.1rem;
+	}
+	.switch{
+		float: right;
+		margin-right: 0.1rem;
+		margin-top: 0.1rem;
+	}
+	.foot{
+		width: 100%;
+		height: 0.7rem;
+		line-height: 0.5rem;
+		background-color: white;
+		padding: 0.08rem;
+		text-align: center;
+		font-size: 0.25rem;
+		color:#ff4949;
+	}
+</style>
--- qidian/src/views/Mine/mineShow.vue
+++ qidian/src/views/Mine/mineShow.vue
@@ -1,15 +1,16 @@
 <template>
+	
 	<div class="mineShow">
 		<!-- 顶部 -->
 		<div class="title">
-			<i class="el-icon-set-up icon-set"></i>
+			<i class="el-icon-set-up icon-set" @click="set"></i>
 			<i class="el-icon-message icon-msg"></i>
 			<div class="change">
 				<i class="el-icon-moon icon-moon"></i>夜间
 			</div>
 		</div>
-		<div class="null"></div>
 		<!-- 个人信息板块 -->
+		<scroll :probeType="3" class="wrapper" ref="scroll" @pullingDown="pullingUp">
 		<div class="people">
 			<div class="details">
 				<div class="details1">
@@ -45,7 +46,7 @@
 				<span>享免费书库等10项福利<i class="el-icon-arrow-right"></i></span>
 			</div>
 			<div class="dredge-bottom">
-				<div v-for="(item,index) in drLists">
+				<div v-for="(item,index) in drLists" :key="index">
 					<i :class='item.icon' class="icon-drlists"></i>{{item.title}}
 				</div>
 			</div>
@@ -72,19 +73,19 @@
 		</div>
 		<!-- 列表板块 -->
 		<div class="foot-list">
-			<div v-for="(item,index) in bottomLists" class="lists" >
+			<div v-for="(item,index) in bottomLists" :key="index" class="lists">
 				<i :class="item.icon1"></i>
 				<span>{{item.content}}</span>
 				<i class="el-icon-arrow-right"></i>
 			</div>
 		</div>
-		<div class="null"></div>
-		
+		</scroll>
 	</div>
 </template>
 
 <script>
-	import navs from '../../components/navs'
+	import scroll from '@/components/common/Scroll/scroll.vue'
+	import navs from '@/components/navs'
 	export default{
 		data(){
 			return{
@@ -121,10 +122,17 @@
 			}
 		},
 		methods:{
-			
+			set(){
+				this.$router.push('/mineSet')
+			},
+			pullingUp(){
+				//console.log(12);
+				this.$refs.scroll.finishPullDown();
+			}
 		},
 		components: {
 		  navs,
+		  scroll
 		},
 		
 	}
@@ -132,10 +140,6 @@
 </script>
 
 <style scoped>
-	*{
-		margin: 0;
-		padding: 0;
-	}
 	.mineShow{
 		background-color: whitesmoke;
 	}
@@ -145,7 +149,7 @@
 		height: 0.7rem;
 		line-height: 0.7rem;
 		background-color: whitesmoke;
-		position: fixed;
+		position: relative;
 		padding: 0.08rem 0.08rem 0.08rem 0.08rem;
 		z-index: 120;
 	}
@@ -176,10 +180,6 @@
 		font-size: 0.4rem;
 		margin-top: 0.05rem;
 		margin-right: 0.1rem;
-	}
-	.null{
-		width: 100%;
-		height: 0.7rem;
 	}
 	/* 个人信息 */
 	.people{
@@ -286,8 +286,7 @@
 	}
 	/* 开通畅享卡板块 */
 	.dredge{
-		width: 94%;
-        height: 2.2rem;
+		width: 94%;        height: 2.2rem;
 		margin-left: 3%;
 		margin-top: -4%;
 		border-radius: 0.15rem;
